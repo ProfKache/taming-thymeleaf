d245d2d8a35d00218b1a3f55c3ba981fa38c8f77
Salim Kachemela <sakachemela@gmail.com>
2024-04-24 10:21:16 +0300

    Add configuration for tailwind

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["../resources/templates/**/*.{html,js}"],
+  content: ["../resources/templates/**/*.{html,js,svg}"],
   theme: {
     extend: {
         colors: {
             'taming-thymeleaf-green':'darkseagreen'
-        }
+        },
+        fontFamily: {
+            sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
