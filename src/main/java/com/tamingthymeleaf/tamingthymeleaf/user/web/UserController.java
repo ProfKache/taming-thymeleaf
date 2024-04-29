4ce6388a3fa00678a0294440919163acb3e81f00
Salim Kachemela <sakachemela@gmail.com>
2024-04-29 09:50:43 +0300

    Add controllers for both team and users

--- src/main/java/com/tamingthymeleaf/tamingthymeleaf/user/web/UserController.java
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/user/web/UserController.java
@@ -1,2 +1,15 @@
-package com.tamingthymeleaf.tamingthymeleaf.user.web;public class UserController {
+package com.tamingthymeleaf.tamingthymeleaf.user.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String index(Model model) {
+        return "users/list";
+    }
 }
