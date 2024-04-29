ba8ff4e438e63fb451d9e72937f2bb5372cca49e
Salim Kachemela <sakachemela@gmail.com>
2024-04-29 09:48:01 +0300

    Add configuration for i18n

--- src/main/java/com/tamingthymeleaf/tamingthymeleaf/infrastructure/web/WebMvcConfiguration.java
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/infrastructure/web/WebMvcConfiguration.java
@@ -1,2 +1,29 @@
-package com.tamingthymeleaf.tamingthymeleaf.infrastructure.web;public class WebMvcConfiguration {
+package com.tamingthymeleaf.tamingthymeleaf.infrastructure.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor() {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        return localeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(localeInterceptor());
+    }
 }
