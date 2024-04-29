9a1e22f6795d7c107d1d90db92fda5dbc446fbb6
Salim Kachemela <sakachemela@gmail.com>
2024-04-29 09:47:07 +0300

    Add root controller for the application

--- src/main/java/com/tamingthymeleaf/tamingthymeleaf/infrastructure/web/RootController.java
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/infrastructure/web/RootController.java
@@ -1,2 +1,14 @@
-package com.tamingthymeleaf.tamingthymeleaf.infrastructure.web;public class RootController {
+package com.tamingthymeleaf.tamingthymeleaf.infrastructure.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RootController {
+    @GetMapping
+    public String root() {
+        return "redirect:/users";
+    }
 }
