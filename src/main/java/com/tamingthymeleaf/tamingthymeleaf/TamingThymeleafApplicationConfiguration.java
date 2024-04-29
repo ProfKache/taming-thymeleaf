408c8d0f162f7de84d31e8540bc597f4728e696e
Salim Kachemela <sakachemela@gmail.com>
2024-04-29 16:55:33 +0300

    Add configuration class for svg files

--- src/main/java/com/tamingthymeleaf/tamingthymeleaf/TamingThymeleafApplicationConfiguration.java
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/TamingThymeleafApplicationConfiguration.java
@@ -1,2 +1,29 @@
-package com.tamingthymeleaf.tamingthymeleaf;public class TamingThymeleafApplicationConfiguration {
+package com.tamingthymeleaf.tamingthymeleaf;
+
+import io.github.wimdeblauwe.jpearl.InMemoryUniqueIdGenerator;
+import io.github.wimdeblauwe.jpearl.UniqueIdGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.UUID;
+
+@Configuration
+public class TamingThymeleafApplicationConfiguration {
+
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+
+        return resolver;
+    }
+
+    @Bean
+    public UniqueIdGenerator<UUID> uniqueIdGenerator (){
+        return new InMemoryUniqueIdGenerator();
+    }
 }
